6bac19ba9498168415b4fb621837f5c7f9fa1651
gabriel silva <breducar7@gmail.com>
2025-06-16 15:08:09 -0300

    Auto-push

---
+++ monacoworker.js
@@ -0,0 +1,5 @@
+// see https://github.com/Microsoft/monaco-editor
+self.MonacoEnvironment = { 
+    baseUrl: "/pxt-microbit-dogo/" 
+};
+importScripts("/pxt-microbit-dogo/vs/base/worker/workerMain.js");
