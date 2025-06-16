faec8e357680fefc3cda4b2ed7f6f1e00a804556
gabriel silva <breducar7@gmail.com>
2025-06-16 15:08:02 -0300

    Initial.

--- monacoworker.js
+++
@@ -1,5 +0,0 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/pxt-microbit-dogo/" 
-};
-importScripts("/pxt-microbit-dogo/vs/base/worker/workerMain.js");
